f4609eda2829041f40715bc24e0ce80561744939
Logan Wohlers <lawohlers@gmail.com>
2019-05-20 09:07:52 -0700

    new repo for frontend

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                TEST2!!
+            </div>
+        )
+    }
+
+}
+
+
+export default MainContainer
