cd6d4f5a4e0528b34e6d32c4d6928a572ce5b4aa
Logan Wohlers <lawohlers@gmail.com>
2019-05-20 19:40:05 -0700

    added router and started setting it up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css';
 import MainMenu from './containers/MainMenu'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <MainMenu />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+          </div>
+        </div>
+
+
+        <Route path="/hello" component={MainMenu} />
+        <Route path="/about" component={MainMenu} />
+        <Route path="/books" component={MainMenu} />
+
+      </Router>
+    )
+  }
 }
 
 export default App;
+
+
