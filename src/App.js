ea9e37975865cc5f0eef5dc5363d202800095f9a
Logan Wohlers <lawohlers@gmail.com>
2019-05-26 18:32:13 -0700

    redux refactor-- fixing route issues

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   Switch
 } from 'react-router-dom'
 
+
 class App extends React.Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {
-    BrowserRouter as Router,
-    Route,
-    withRouter,
-    Link,
-    Switch,
-    Redirect
-} from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const store = createStore(reducers)
+
+//wrap app in provider. get rid of this router trash
 
 ReactDOM.render(
     <Router>
@@ -18,9 +17,3 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root')
 );
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
