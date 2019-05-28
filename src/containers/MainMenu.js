3f31db8fb13fa7a211e579f648d19c47438641bf
Logan Wohlers <lawohlers@gmail.com>
2019-05-28 11:11:37 -0700

    very slight update on homepage styling cards

--- src/containers/MainMenu.js
+++ src/containers/MainMenu.js
@@ -3,27 +3,26 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setSeason } from '../actions'
 
-import giannis from '../assets/giannis.jpg'
-import harden from '../assets/harden.jpg'
-
 class MainMenu extends React.Component {
 
     render() {
         return (
-            <div>
-                <img className='homePage' style={{ width: 300, height: 300 }} src={giannis} alt='' />
-                <img className='homePage' style={{ width: 300, height: 300 }} src={harden} alt='' />
-                <ul>
-                    {this.props.seasons.map((season, idx) => {
-                        let year = season.year
-                        return (
-                            <li key={idx} onClick={() => this.props.setSeason(year)} >
-                                <Link to={`/seasonhome/${year}`}>
-                                    {`${year - 1}-${year} Season`} </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
+            <div className='main container'>
+
+                {this.props.seasons.map((season, idx) => {
+                    let year = season.year
+                    return (
+                        <div className='ui card' key={idx} onClick={() => this.props.setSeason(year)} >
+
+
+                            <Link to={`/seasonhome/${year}`}>
+                                <img className='image' style={{ width: 300, height: 300 }} src={season.image} alt='' />
+                                {`${year - 1}-${year} Season`} </Link>
+
+                        </div>
+                    )
+                })}}
+
             </div >
 
         )
