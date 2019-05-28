0af89c492a16cfe047c5d71af2c4b17ff0bb5274
Logan Wohlers <lawohlers@gmail.com>
2019-05-28 12:21:30 -0700

    added victory chart set up

--- src/containers/MainMenu.js
+++ src/containers/MainMenu.js
@@ -2,36 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setSeason } from '../actions'
+import { VictoryBar, VictoryChart } from 'victory';
 
 class MainMenu extends React.Component {
 
     render() {
         return (
             <div className='main container'>
+                <VictoryChart domainPadding={20}>
+                    <VictoryBar />
+                </VictoryChart>
 
-                {this.props.seasons.map((season, idx) => {
-                    let year = season.year
-                    return (
-                        <div className='ui card' key={idx} onClick={() => this.props.setSeason(year)} >
+                    {this.props.seasons.map((season, idx) => {
+                        let year = season.year
+                        return (
+                            <div className='ui card' key={idx} onClick={() => this.props.setSeason(year)} >
 
 
-                            <Link to={`/seasonhome/${year}`}>
-                                <img className='image' style={{ width: 300, height: 300 }} src={season.image} alt='' />
-                                {`${year - 1}-${year} Season`} </Link>
+                                <Link to={`/seasonhome/${year}`}>
+                                    <img className='image' style={{ width: 300, height: 300 }} src={season.image} alt='' />
+                                    {`${year - 1}-${year} Season`} </Link>
 
-                        </div>
-                    )
-                })}}
-
+                            </div>
+                        )
+                    })}}
+    
             </div >
 
-        )
-    }
-}
-
+                )
+            }
+        }
+        
 const mapStateToProps = (state) => {
     console.log(state)
-    return { seasons: state.allSeasons }
+    return {seasons: state.allSeasons }
 }
-
-export default connect(mapStateToProps, { setSeason })(MainMenu)
+            
+export default connect(mapStateToProps, {setSeason})(MainMenu)
