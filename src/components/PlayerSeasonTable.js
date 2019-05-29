c3e0af082c2c8ef768e89203ccc45d74e0bb50aa
Logan Wohlers <lawohlers@gmail.com>
2019-05-28 21:36:20 -0700

    leaving off here-- need to fix styling/formatting tomorrow

--- src/components/PlayerSeasonTable.js
+++ src/components/PlayerSeasonTable.js
@@ -5,46 +5,48 @@ class PlayerSeasonTable extends React.Component {
 
     render() {
         return (
-            <table id="PlayerSeason" className="">
-                <thead className="ui selectable inverted table">
-                    <tr>
-                        <th>Player</th>
-                        <th>Pos</th>
-                        <th>Age</th>
-                        <th>Tm</th>
-                        <th>G</th>
-                        <th>MP</th>
-                        <th>FG</th>
-                        <th>FGA</th>
-                        <th>FG%</th>
-                        <th>3P</th>
-                        <th>3PA</th>
-                        <th>3P%</th>
-                        <th>2P</th>
-                        <th>2PA</th>
-                        <th>2P%</th>
-                        <th>eFG%</th>
-                        <th>FT</th>
-                        <th>FTA</th>
-                        <th>FT%</th>
-                        <th>ORB</th>
-                        <th>DRB</th>
-                        <th>TRB</th>
-                        <th>AST</th>
-                        <th>STL</th>
-                        <th>BLK</th>
-                        <th>TOV</th>
-                        <th>PF</th>
-                        <th>PTS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.playerSeasons.map((ps, idx) => {
-                        return <PlayerSeasonRow id={ps.id} key={idx} />
-                    })}
+            <div className='ui container'>
+                <table id="PlayerSeason" className="ui selectable inverted table">
+                    <thead >
+                        <tr>
+                            <th>Player</th>
+                            <th>Pos</th>
+                            <th>Age</th>
+                            <th>Tm</th>
+                            <th>G</th>
+                            <th>MP</th>
+                            <th>FG</th>
+                            <th>FGA</th>
+                            <th>FG%</th>
+                            <th>3P</th>
+                            <th>3PA</th>
+                            <th>3P%</th>
+                            <th>2P</th>
+                            <th>2PA</th>
+                            <th>2P%</th>
+                            <th>eFG%</th>
+                            <th>FT</th>
+                            <th>FTA</th>
+                            <th>FT%</th>
+                            <th>ORB</th>
+                            <th>DRB</th>
+                            <th>TRB</th>
+                            <th>AST</th>
+                            <th>STL</th>
+                            <th>BLK</th>
+                            <th>TOV</th>
+                            <th>PF</th>
+                            <th>PTS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.playerSeasons.map((ps, idx) => {
+                            return <PlayerSeasonRow id={ps.id} key={idx} />
+                        })}
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         )
 
 
